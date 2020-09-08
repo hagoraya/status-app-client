b0238675108e494c814cd28e1c1188eb0068c2f8
Harry <harpreetgoraya95@gmail.com>
2020-09-08 15:19:29 -0400

    fixed apollo 400 error when deleteing post

--- src/components/DeleteButton.js
+++ src/components/DeleteButton.js
@@ -7,78 +7,68 @@ import { FETCH_POSTS_QUERY } from './../util/graphql'
 import MyPopup from '../util/MyPopup'
 
 function DeleteButton({ postId, commentId, callback }) {
+    const [confirmOpen, setConfirmOpen] = useState(false);
 
-    const [confirmOpen, setConfirmOpen] = useState(false)
-
-
-    //if commentId then we are deleting a comment and not a post
-    const mutation = commentId ? DELETE_COMMENT_MUTATION : DELETE_POST_MUTATION
+    const mutation = commentId ? DELETE_COMMENT_MUTATION : DELETE_POST_MUTATION;
 
     const [deletePostOrMutation] = useMutation(mutation, {
         update(proxy) {
             setConfirmOpen(false);
-
             if (!commentId) {
+                //remove post from cache
                 const data = proxy.readQuery({
                     query: FETCH_POSTS_QUERY
                 });
-
-                proxy.writeQuery({
-                    query: FETCH_POSTS_QUERY,
-                    data: {
-                        getPosts: data.getPosts.filter(p => p.id !== postId)
-                    }
-                })
+                data.getPosts = data.getPosts.filter((p) => p.id !== postId);
+                proxy.writeQuery({ query: FETCH_POSTS_QUERY, data });
             }
-
             if (callback) callback();
         },
         variables: {
-            postId, commentId
+            postId,
+            commentId
         }
     });
-
-
     return (
         <>
-            <MyPopup content={commentId ? 'Delete Comment' : 'Delete Post'}>
-                <Button as="div"
-                    color='red'
-                    onClick={() => setConfirmOpen(true)}
+            <MyPopup content={commentId ? 'Delete comment' : 'Delete post'}>
+                <Button
+                    as="div"
+                    color="red"
                     floated="right"
+                    onClick={() => setConfirmOpen(true)}
                 >
-                    <Icon name='trash' style={{ margin: 0 }}></Icon>
+                    <Icon name="trash" style={{ margin: 0 }} />
                 </Button>
             </MyPopup>
-
             <Confirm
                 open={confirmOpen}
                 onCancel={() => setConfirmOpen(false)}
-                onConfirm={deletePostOrMutation}>
-            </Confirm>
+                onConfirm={deletePostOrMutation}
+            />
         </>
-    )
-
+    );
 }
 
 const DELETE_POST_MUTATION = gql`
-    mutation deletePostOrMutation($postId: ID!){
-        deletePostOrMutation(postId: $postId)
+    mutation deletePost($postId: ID!) {
+      deletePost(postId: $postId)
     }
-
-
-`
+  `;
 
 const DELETE_COMMENT_MUTATION = gql`
-    mutation deleteComment($postId: ID!, $commentId: ID!){
-        deleteComment(postId: $postId, commentId: $commentId){
-            id
-            comments{
-                id username createdAt body
-            }
-            commentCount
+    mutation deleteComment($postId: ID!, $commentId: ID!) {
+      deleteComment(postId: $postId, commentId: $commentId) {
+        id
+        comments {
+          id
+          username
+          createdAt
+          body
         }
+        commentCount
+      }
     }
-`
+  `;
 
-export default DeleteButton
+export default DeleteButton;
